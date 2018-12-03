0c674e499293a2bf41ead56e9336fc1add0f00f1
avallonking <lijj36@mail2.sysu.edu.cn>
2018-12-03 00:52:55 -0800

    update compute_gc

--- ForestQC/get_ref_genome_GC.py
+++ ForestQC/get_ref_genome_GC.py
@@ -11,34 +11,36 @@ import numpy as np
 
 
 def computeGC(seq):
-  try:
-    gc = (seq.count('G') + seq.count('C')) / (seq.count('G') + seq.count('C') + seq.count('A') + seq.count('T'))
-    return gc
-  except ZeroDivisionError:
-    return np.nan
+    try:
+        gc = (seq.count('G') + seq.count('C')) / (seq.count('G') + seq.count('C') + seq.count('A') + seq.count('T'))
+        return gc
+    except ZeroDivisionError:
+        return np.nan
 
 
 def execute_compute_gc(ref, out, window_size):
-  # get the reference genome
-  r = open(ref, 'r')
-  o = open(out, 'w')
+    # get the reference genome
+    r = open(ref, 'r')
+    o = open(out, 'w')
 
-  seq = ''
-  start_pos = 1
-  for line in r:
-    if line.startswith('>'):
-      if len(seq) != 0:
-        gc = computeGC(seq)
-        seq = ''
-        o.write('\t'.join([chr, str(start_pos), str(gc)]) + '\n')
-      chr = line.strip()[1:]
-      start_pos = 1
-    else:
-      seq += line.strip().upper()
-      if len(seq) >= window_size:
-        gc = computeGC(seq)
-        seq = ''
-        o.write('\t'.join([chr, str(start_pos), str(gc)]) + '\n')
-        start_pos += window_size
-  r.close()
-  o.close()
+    seq = ''
+    start_pos = 1
+    for line in r:
+        if line.startswith('>'):
+            if len(seq) != 0:
+                gc = computeGC(seq)
+                seq = ''
+                o.write('\t'.join([chr, str(start_pos), str(gc)]) + '\n')
+            chr = line.split()[0][1:]
+            if 'chr' not in chr:
+                chr = 'chr' + chr
+            start_pos = 1
+        else:
+            seq += line.strip().upper()
+            if len(seq) >= window_size:
+                gc = computeGC(seq)
+                seq = ''
+                o.write('\t'.join([chr, str(start_pos), str(gc)]) + '\n')
+                start_pos += window_size
+    r.close()
+    o.close()
